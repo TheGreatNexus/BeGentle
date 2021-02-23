89864f40cfc92fdebd3861740dc440791402da88
Kazzbo <cedricaadam@gmail.com>
2021-02-23 21:40:15 +0100

    Premier Jet / buggé - Manager full implementation -
    Le bug est présent tant que le player spawn n'est pas défini ( donc le cycle de vie).

---
+++ Assets/Scripts/General/SingletonGameStateObserver.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SDD.Events;
+using System;
+
+public abstract class SingletonGameStateObserver<T> : Singleton<T>, IEventHandler where T : Component
+{
+	public virtual void SubscribeEvents()
+	{
+		EventManager.Instance.AddListener<GameMenuEvent>(GameMenu);
+		EventManager.Instance.AddListener<GamePlayEvent>(GamePlay);
+		EventManager.Instance.AddListener<GamePauseEvent>(GamePause);
+		EventManager.Instance.AddListener<GameResumeEvent>(GameResume);
+	}
+
+	public virtual void UnsubscribeEvents()
+	{
+		EventManager.Instance.RemoveListener<GameMenuEvent>(GameMenu);
+		EventManager.Instance.RemoveListener<GamePlayEvent>(GamePlay);
+		EventManager.Instance.RemoveListener<GamePauseEvent>(GamePause);
+		EventManager.Instance.RemoveListener<GameResumeEvent>(GameResume);
+	}
+
+	protected override void Awake()
+	{
+		base.Awake();
+		SubscribeEvents();
+	}
+
+	protected virtual void OnDestroy()
+	{
+		UnsubscribeEvents();
+	}
+
+	protected virtual void GameMenu(GameMenuEvent e)
+	{
+	}
+
+	protected virtual void GamePlay(GamePlayEvent e)
+	{
+	}
+
+	protected virtual void GamePause(GamePauseEvent e)
+	{
+	}
+
+	protected virtual void GameResume(GameResumeEvent e)
+	{
+	}
+}
