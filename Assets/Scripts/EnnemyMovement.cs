e5bb4dfe6d41fbd5c0164f8eb4d909358815340b
LAPTOP-6RV07JK4\shas <n.farjon.pro@gmail.com>
2021-03-16 17:18:53 +0100

    Script mouvement ennemie

---
+++ Assets/Scripts/EnnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnnemyMovement : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    //[SerializeField] private Object player;
+    //[SerializeField] private Vector3 playerlocation;
+    [SerializeField] private GameObject player;
+    //private Vector3 playerlocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //        playerlocation = player.transform.postion;
+
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Debug.Log(agent.transform);
+        agent.SetDestination(player.transform.position);
+
+
+    }
+}
