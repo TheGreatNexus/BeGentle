9bf729608c85b2a4497f364e5eaae97c6890f739
TheGreatNexus <theo-ozanic@hotmail.fr>
2021-06-28 15:30:03 +0200

    added code

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,30 +5,56 @@ using SDD.Events;
 
 public class Enemy : MonoBehaviour, IHit
 {
-    float m_CooldownHit = 0.1f;
-    float m_NextHit = 0;
-    public float m_enemyHp = 30;
+    [SerializeField] float m_EnemyHp;
+    [SerializeField] int m_EnemyDamages;
+    [SerializeField] int m_EnemySpeed;
+    private enum enemyState { IDLE, RUNNING, CHARGING, HITTED, DEAD };
+    private enemyState m_EnemyState;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (m_enemyHp <= 0)
+        if(m_EnemyState == enemyState.HITTED){
+
+        }
+        if (m_EnemyHp <= 0 && anim.GetBool("IsDead") == false)
         {
-            EventManager.Instance.Raise(new PlayerHasKilledEnemyEvent());
-            Destroy(gameObject);
+            m_EnemyState = enemyState.DEAD;
+            anim.SetBool("IsDead", true);
+            StartCoroutine(enemyIsDead());
         }
     }
 
     public void Hit(float damage)
     {
-         if (Time.fixedTime > m_NextHit)
-         {   Debug.Log(m_enemyHp);
-             m_NextHit = Time.fixedTime + m_CooldownHit;
-            m_enemyHp -= damage;
-         }else{
-             Debug.Log("Time : " + Time.fixedTime + "\nNext Hit Time : " + m_NextHit);
-             Debug.Log(m_enemyHp);
-         }
+        m_EnemyState = enemyState.HITTED;
+        anim.SetTrigger("HasBeenHitted");
+        m_EnemyHp -= damage;
+        //  }else{
+        //      Debug.Log("Time : " + Time.fixedTime + "\nNext Hit Time : " + m_NextHit);
+        //      Debug.Log(m_enemyHp);
+        //  }
+    }
+
+    private void animationChecker(Animator anim){
+
+    }
+    IEnumerator enemyIsDead()
+    {
+        if (m_EnemyState == enemyState.DEAD)
+        {
+            yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+            Debug.Log("anim ended");
+            EventManager.Instance.Raise(new PlayerHasKilledEnemyEvent());
+            Destroy(gameObject);
+        }
     }
 
 }
