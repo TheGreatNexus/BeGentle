a80c32b57f4614331a3798d7822dbd43b23e8dbf
TheGreatNexus <theo-ozanic@hotmail.fr>
2021-07-14 14:09:47 +0200

    fixed collisions bugs

--- Assets/Scripts/General/MouseLook.cs
+++ Assets/Scripts/General/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    float m_MouseSensitivity = 100f;
+    public float m_MouseSensitivity = 100f;
     float m_XRotation = 0f;
 
     public Transform m_PlayerBody;
